3bfaa98dacb97d96ba5203caf2b66b3453085802
kmrsensk <62285305+kmrsensk@users.noreply.github.com>
2020-07-30 14:34:07 +0900

    Update test-5-14.c

--- prog/test-5-14.c
+++ prog/test-5-14.c
@@ -2,7 +2,7 @@
 int main()
 {
     /*
-    課題1
+    kadai1
     int  x=8,y=2,wa,sa,seki,syou,amari;
     printf(" x=%d\n wa=%d\n sa=%d\n seki=%d\n syou=%d\n amari=%d\n",x,x+y,x-y,x*y,x/y,x%y);
     return 0;
@@ -13,6 +13,6 @@ int month=1,day=16,a,b,c,d,e;
     c=b*50;
     d=c+day;
     e=d-250;
-    printf("誕生日:%d月%d日\n計算結果:%d\n*****\n計算過程\n(1)%d\n(2)%d\n(3)%d\n(4)%d\n(5)%d\n",month,day,e,a,b,c,d,e);
+    printf("birthday:%d /%d \anser:%d\n*****\n \n(1)%d\n(2)%d\n(3)%d\n(4)%d\n(5)%d\n",month,day,e,a,b,c,d,e);
     return 0;
 }
